726ac579683e8e25c8be6430c7b74541f9e31ec7
Bailey-Lievens <baileylievens@hotmail.be>
2025-05-25 17:30:31 +0200

    Fix: Pathing for online

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-    fetch('./../data.json')
+    fetch('../data.json')
     .then((response) => response.json())
     .then((data) => handleData(data));
 });
